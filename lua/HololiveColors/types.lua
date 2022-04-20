18ebc7758d3d2007e80f61754c2a5a97a21a1c8a
eraser5th <hisanniwo0202@gmail.com>
2022-04-20 19:46:31 +0900

    add types file to declare types

---
+++ lua/HololiveColors/types.lua
@@ -0,0 +1,8 @@
+---@alias Style "bold" | "italic" | "underline" | "undercurl" | "strikethrough" | "inverse" | "none"
+
+--- @class Color
+  --- @field fg string
+  --- @field bg string
+  --- @field sp string
+  --- @field style Style
+
