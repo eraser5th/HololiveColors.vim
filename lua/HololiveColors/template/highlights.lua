b3112a680731bafe07148f00e53667470e53e079
eraser5th <hisanniwo0202@gmail.com>
2022-04-22 12:27:52 +0900

    templateはいらなそうなので消去

--- lua/HololiveColors/template/highlights.lua
+++
@@ -1,9 +0,0 @@
-local colors = require("HololiveColors.tokoyami-towa.colors")
-
-local highlights = {
---GroupName   = { fg = color       ,  bg = color,          sp = color,         style = Style },
-  Normal      = { fg = colors.white,  bg = colors.bgDark,                                    },
-  CursorLine  = {                     bg = colors.bgLight,                                   },
-}
-
-return highlights
