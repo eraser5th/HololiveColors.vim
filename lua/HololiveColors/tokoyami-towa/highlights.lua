5026028fe57d715a59d7217807bd7d1f312c7eee
eraser5th <hisanniwo0202@gmail.com>
2022-04-22 12:23:59 +0900

    add colors and highlights

--- lua/HololiveColors/tokoyami-towa/highlights.lua
+++ lua/HololiveColors/tokoyami-towa/highlights.lua
@@ -1,9 +1,17 @@
 local colors = require("HololiveColors.tokoyami-towa.colors")
 
 local highlights = {
---GroupName   = { fg = color       ,  bg = color,          sp = color,         style = Style },
-  Normal      = { fg = colors.white,  bg = colors.bgDark,                                    },
-  CursorLine  = {                     bg = colors.bgLight,                                   },
+--GroupName       = { fg = color,             bg = color,          sp = color,         style = Style },
+  Normal          = { fg = colors.white,      bg = colors.bgDark,                                    },
+  CursorLine      = {                         bg = colors.bgLight,                                   },
+  Comment         = { fg = colors.comment,                                                           },
+  Constant        = { fg = colors.orange,                                                            },
+  Special         = { fg = colors.purple,                                                            },
+  Type            = { fg = colors.pink,                                                              },
+  Identifier      = { fg = colors.purple,                                                            },
+  Statement       = { fg = colors.lightGreen,                                                        },
+  Underlined      = { fg = colors.orange,                                                            },
+  Error           = { fg = colors.red,                                                               },
 }
 
 return highlights
